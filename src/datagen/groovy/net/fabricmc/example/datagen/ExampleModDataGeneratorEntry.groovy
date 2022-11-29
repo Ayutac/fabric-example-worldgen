84c9566b6d02ae34a11bdfaac15cc91d14832859
ayutac <fly.high.android@gmail.com>
2022-11-29 09:32:23 +0100

    added datagen

---
+++ src/datagen/groovy/net/fabricmc/example/datagen/ExampleModDataGeneratorEntry.groovy
@@ -0,0 +1,13 @@
+package net.fabricmc.example.datagen
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+import org.slf4j.LoggerFactory
+
+class ExampleModDataGeneratorEntry implements DataGeneratorEntrypoint {
+
+    @Override
+    void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
